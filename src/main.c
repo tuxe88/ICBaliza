6845b7064d80c40fdc2f7fa4152e5edc41201015
tuxe88 <v.tourrilhes@gmail.com>
2023-10-05 09:03:15 -0300

    se puede hacer request para ver los workflows!

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <string.h> //for handling strings
 #include <stdlib.h>
+#include <stddef.h>
 #include <sys/param.h>
 #include <stdio.h> //for basic printf commands
 #include "freertos/FreeRTOS.h" //for delay,mutexs,semphrs rtos operations
@@ -11,11 +12,16 @@
 #include "lwip/err.h" //light weight ip packets error handling
 #include "secrets.h"
 #include "esp_tls_crypto.h"
+#include "esp_tls.h"
 #include "protocol_examples_common.h"
 #include "protocol_examples_utils.h"
+#include "esp_crt_bundle.h"
+
 #include <esp_http_server.h>
+#include "esp_http_client.h"
 
 int retry_num = 0;
+int build_state = 0;
 
 #define CONFIG_EXAMPLE_BASIC_AUTH 1
 
@@ -299,7 +305,7 @@ static esp_err_t state_post_handler(httpd_req_t *req)
 {
     char buf[100];
     int ret, remaining = req->content_len;
-
+    int newState = 0;
     while (remaining > 0) {
         /* Read the data for the request */
         if ((ret = httpd_req_recv(req, buf,
@@ -517,6 +523,90 @@ esp_wifi_connect();                   // connect with saved ssid and pass
 
 }
 
+esp_err_t client_event_get_handler(esp_http_client_event_t *evt){
+
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        /*
+         *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+         *  However, event handler can also be used in case chunked encoding is used.
+         */
+        if (!esp_http_client_is_chunked_response(evt->client)) {
+            // If user_data buffer is configured, copy the response into the buffer
+            if (evt->user_data) {
+                memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+            } else {
+                if (output_buffer == NULL) {
+                    output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                    output_len = 0;
+                    if (output_buffer == NULL) {
+                        ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                        return ESP_FAIL;
+                    }
+                }
+                memcpy(output_buffer + output_len, evt->data, evt->data_len);
+            }
+            output_len += evt->data_len;
+        }
+
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
+        if (output_buffer != NULL) {
+            // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+            printf("%s", output_buffer);
+            free(output_buffer);
+            output_buffer = NULL;
+        }
+        output_len = 0;
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+        int mbedtls_err = 0;
+        esp_err_t err = esp_tls_get_and_clear_last_error(evt->data, &mbedtls_err, NULL);
+        if (err != 0) {
+            ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+            ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+        }
+        if (output_buffer != NULL) {
+            free(output_buffer);
+            output_buffer = NULL;
+        }
+        output_len = 0;
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+char responseBuffer[512];
+
+static void get_workflows_github(){
+    esp_http_client_config_t config_get = {
+        .url = "https://api.github.com/repos/tuxe88/dyasc-2023/actions/runs?per_page=1&page=1",
+        .method = HTTP_METHOD_GET,
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,  //Specify transport type
+        .crt_bundle_attach = esp_crt_bundle_attach, //Attach the certificate bundle 
+        .event_handler = client_event_get_handler  
+    };
+
+    esp_http_client_handle_t client = esp_http_client_init(&config_get);
+    //char *post_data = "{\"saa\":\"dsdssdadsa\"}";
+    //esp_http_client_set_post_field(client, post_data, strlen(post_data));
+    esp_http_client_set_header(client,"Content-Type","application/vnd.github+json");
+    esp_http_client_set_header(client,"Authorization","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");
+    esp_http_client_set_header(client,"X-GitHub-Api-Version","2022-11-28");
+
+    esp_http_client_perform(client);
+    esp_http_client_cleanup(client);    
+}
+
 void app_main()
 {
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -543,8 +633,9 @@ void app_main()
     
     /* Start the server for the first time */
     server = start_webserver();
-
     while (server) {
-        sleep(5);
+        printf("build state %d\n",build_state);
+        get_workflows_github();
+        sleep(15);
     }
 }
