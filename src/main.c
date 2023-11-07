7ef7c2e6eb5ffcf1aa9ea19f142213e6dfb4b937
tuxe88 <v.tourrilhes@gmail.com>
2023-11-06 22:17:22 -0300

    Version estable con beep, fix de cantidad de requests y titilado de led

--- src/main.c
+++ src/main.c
@@ -422,22 +422,21 @@ esp_err_t client_event_get_handler(esp_http_client_event_t *evt){
     return ESP_OK;
 }
 
-char responseBuffer[512];
-
 static void get_workflows_github(){
     esp_http_client_config_t config_get = {
         .url = "https://api.github.com/repos/"REPO_OWNER"/"REPO_NAME"/actions/runs?per_page=1&page=1",
         .method = HTTP_METHOD_GET,
         .transport_type = HTTP_TRANSPORT_OVER_SSL,  //Specify transport type
         .crt_bundle_attach = esp_crt_bundle_attach, //Attach the certificate bundle 
-        .event_handler = client_event_get_handler  
+        .event_handler = client_event_get_handler,
+        .buffer_size = 512
     };
 
     esp_http_client_handle_t client = esp_http_client_init(&config_get);
     //char *post_data = "{\"saa\":\"dsdssdadsa\"}";
     //esp_http_client_set_post_field(client, post_data, strlen(post_data));
     esp_http_client_set_header(client,"Content-Type","application/vnd.github+json");
-    esp_http_client_set_header(client,"Authorization","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");
+    esp_http_client_set_header(client,"Authorization","Bearer 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");
     esp_http_client_set_header(client,"X-GitHub-Api-Version","2022-11-28");
 
     esp_http_client_perform(client);
@@ -510,8 +509,8 @@ void app_main()
                 gpio_set_level(LED_AZUL, 0);
                 gpio_set_level(LED_ROJO, 1);
                 if(previous_state==STATE_BUILD_SUCCESS){
-                    stateChangedLed(LED_ROJO);
                     beepBuzzer();
+                    stateChangedLed(LED_ROJO);
                 }
                 break;
             
@@ -528,8 +527,8 @@ void app_main()
                 gpio_set_level(LED_AZUL, 0);
                 gpio_set_level(LED_ROJO, 0);
                 if(previous_state==STATE_BUILD_FAILED){
-                    stateChangedLed(LED_VERDE);
                     beepBuzzer();
+                    stateChangedLed(LED_VERDE);
                 }
                 break;
 
@@ -540,8 +539,10 @@ void app_main()
                 break;
 
         }
-
-        vTaskDelay(DELAY_TIME*15 / portTICK_PERIOD_MS);
+        //consulta cada 18 miuntos
+        //vTaskDelay(DELAY_TIME*1080 / portTICK_PERIOD_MS);
+        //consulta cada 10 sec
+        vTaskDelay(DELAY_TIME*10 / portTICK_PERIOD_MS);
     }
 
     /*server = start_webserver();
